34df4a24bc20ac09b4916ab52a40f510a61f5132
Maxim Oransky <maxim.oransky@gmail.com>
2020-09-11 21:44:19 +0300

    jsoniter test

--- event_test.go
+++ event_test.go
@@ -4,13 +4,42 @@ import (
 	"encoding/json"
 	"testing"
 
+	jsoniter "github.com/json-iterator/go"
+
 	uuid "github.com/satori/go.uuid"
 )
 
+// BenchmarkSerialization/json-12         	  223381	      5204 ns/op	    1834 B/op	      21 allocs/op
+// BenchmarkSerialization/jsoniter-12     	  394774	      3073 ns/op	    1546 B/op	      18 allocs/op
 func BenchmarkSerialization(b *testing.B) {
-	b.ReportAllocs()
-
 	b.Run("json", func(b *testing.B) {
+		b.ReportAllocs()
+		for i := 0; i < b.N; i++ {
+			event := NewClientMessageUpdated(ClientMessageUpdatedParams{
+				Content:   MessageContent{Text: "test 123"},
+				MessageId: uuid.NewV4().String(),
+				Important: true,
+			})
+
+			data, err := json.Marshal(event)
+			if err != nil {
+				b.Fatal(err)
+			}
+
+			v := new(ClientMessageUpdated)
+			if err := json.Unmarshal(data, v); err != nil {
+				b.Fatal(err)
+			}
+
+			if v.Params.MessageId != event.Params.MessageId {
+				b.Fatal("message id mismatchs")
+			}
+		}
+	})
+
+	b.Run("jsoniter", func(b *testing.B) {
+		b.ReportAllocs()
+		var json = jsoniter.ConfigCompatibleWithStandardLibrary
 		for i := 0; i < b.N; i++ {
 			event := NewClientMessageUpdated(ClientMessageUpdatedParams{
 				Content:   MessageContent{Text: "test 123"},
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/tada-team/tdproto
 
 go 1.14
 
-require github.com/satori/go.uuid v1.2.0
+require (
+	github.com/json-iterator/go v1.1.10
+	github.com/satori/go.uuid v1.2.0
+)
