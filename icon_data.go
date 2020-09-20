1873d28a42afc6d971d27d6ad0c7eb69cc36f739
Maxim Oransky <maxim.oransky@gmail.com>
2020-09-20 14:41:47 +0300

    icon_data docs

--- icon_data.go
+++ icon_data.go
@@ -1,17 +1,33 @@
 package tdproto
 
+// Small or large icon
 type SingleIcon struct {
-	Url    string `json:"url,omitempty"`
-	Width  int    `json:"width,omitempty"`
-	Height int    `json:"height"`
+	// absolute url to icon
+	Url string `json:"url"`
+
+	// Icon width, in pixels
+	Width int `json:"width"`
+
+	// Icon height, in pixels
+	Height int `json:"height"`
 }
 
+// Icon data. Contains sm+lg (for uploaded image) OR stub+letters+color (for icon generated from display name)
 type IconData struct {
-	Sm      *SingleIcon `json:"sm,omitempty"`
-	Lg      *SingleIcon `json:"lg,omitempty"`
-	Stub    string      `json:"stub,omitempty"`
-	Letters string      `json:"letters,omitempty"`
-	Color   string      `json:"color,omitempty"`
+	// Small icon
+	Sm *SingleIcon `json:"sm,omitempty"`
+
+	// Large image
+	Lg *SingleIcon `json:"lg,omitempty"`
+
+	// Generated image with 1-2 letters
+	Stub string `json:"stub,omitempty"`
+
+	// Letters from stub icon
+	Letters string `json:"letters,omitempty"`
+
+	// Stub icon background color
+	Color string `json:"color,omitempty"`
 }
 
 func (d *IconData) SmUrlOrStub() string {
