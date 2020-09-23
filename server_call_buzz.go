5281192fb19f4e57402845f98ea6f9e7f0bc0d8d
Maxim Oransky <maxim.oransky@gmail.com>
2020-09-23 03:17:41 +0300

    codestyle

--- server_call_buzz.go
+++ server_call_buzz.go
@@ -4,35 +4,36 @@ import (
 	"time"
 )
 
-func NewServerCallBuzz(teamShort TeamShort, chatShort ChatShort, actorShort ContactShort, uid string, timeout time.Duration) (resp ServerCallBuzz) {
-	resp.BaseEvent.Name = "server.call.buzz"
-	resp.Params.TeamShort = teamShort
-	resp.Params.ChatShort = chatShort
-	resp.Params.ActorShort = actorShort
-	resp.Params.Uid = uid
-	resp.Params.Jid = chatShort.Jid
-	resp.Params.BuzzTimeout = int(timeout.Seconds())
+func NewServerCallBuzz(teamShort TeamShort, chatShort ChatShort, actorShort ContactShort, uid string, timeout time.Duration) (r ServerCallBuzz) {
+	r.BaseEvent.Name = "server.call.buzz"
+	r.Params.TeamShort = teamShort
+	r.Params.ChatShort = chatShort
+	r.Params.ActorShort = actorShort
+	r.Params.Uid = uid
+	r.Params.Jid = chatShort.Jid
+	r.Params.BuzzTimeout = int(timeout.Seconds())
 
-	resp.Params.Icons = chatShort.Icons
+	r.Params.Icons = chatShort.Icons
 
-	// Set as Deprecated or just leave for reverse compatibility?
-	resp.Params.Team = teamShort.Uid
-	resp.Params.DisplayName = chatShort.DisplayName
-	return resp
+	// Set as Deprecated or just leave for backward compatibility?
+	r.Params.Team = teamShort.Uid
+	r.Params.DisplayName = chatShort.DisplayName
+	return r
 }
 
 type ServerCallBuzz struct {
 	BaseEvent
-	Params struct {
-		TeamShort   TeamShort    `json:"teaminfo"`
-		ChatShort   ChatShort    `json:"chat"`
-		ActorShort  ContactShort `json:"actor"`
-		Uid         string       `json:"uid"`
-		Jid         JID          `json:"jid"`
-		BuzzTimeout int          `json:"buzz_timeout"`
-		Icons       *IconData    `json:"icons"`
+	Params serverCallBuzzParams `json:"params"`
+}
 
-		Team        string `json:"team"`
-		DisplayName string `json:"display_name"`
-	} `json:"params"`
+type serverCallBuzzParams struct {
+	TeamShort   TeamShort    `json:"teaminfo"`
+	ChatShort   ChatShort    `json:"chat"`
+	ActorShort  ContactShort `json:"actor"`
+	Uid         string       `json:"uid"`
+	Jid         JID          `json:"jid"`
+	BuzzTimeout int          `json:"buzz_timeout"`
+	Icons       *IconData    `json:"icons"`
+	Team        string       `json:"team"`
+	DisplayName string       `json:"display_name"`
 }
