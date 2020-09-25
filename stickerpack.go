6c13a4a248828e642951d4218a1659ee00fdcf7c
Maxim Oransky <maxim.oransky@gmail.com>
2020-09-26 02:28:53 +0300

    sticker fields

---
+++ stickerpack.go
@@ -0,0 +1,19 @@
+package tdproto
+
+type Stickerpack struct {
+	Uid        string    `json:"uid"`
+	Name       string    `json:"name"`
+	Title      string    `json:"title"`
+	Author     string    `json:"author,omitempty"`
+	AuthorLink string    `json:"author_link,omitempty"`
+	Stickers   []Sticker `json:"stickers"`
+}
+
+type Sticker struct {
+	Uid            string         `json:"uid"`
+	Icon64         string         `json:"icon64"`
+	Icon100        string         `json:"icon100"`
+	Icon128        string         `json:"icon128"`
+	Icon200        string         `json:"icon200"`
+	MessageContent MessageContent `json:"message_content"`
+}
