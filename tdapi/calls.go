88b73f7c51e1be47248358ded0ba95d395ae8138
E.Kalinin <shoreward@gmail.com>
2020-10-01 15:20:14 +0300

    format

--- tdapi/calls.go
+++ tdapi/calls.go
@@ -2,27 +2,26 @@ package tdapi
 
 import "github.com/tada-team/tdproto"
 
-
 type ClientCallTrickle struct {
-	Jid           tdproto.JID    `json:"jid"`
-	Candidate     string `json:"candidate"`
-	SdpMid        string `json:"sdp_mid"`
-	SdpMlineIndex int    `json:"sdp_mline_index"`
+	Jid           tdproto.JID `json:"jid"`
+	Candidate     string      `json:"candidate"`
+	SdpMid        string      `json:"sdp_mid"`
+	SdpMlineIndex int         `json:"sdp_mline_index"`
 }
 
 type ClientCallRecord struct {
-	Jid           tdproto.JID    `json:"jid"`
-	Audiorecord bool         `json:"audiorecord,omitempty"`
-	Uid         string       `json:"uid"`
+	Jid         tdproto.JID `json:"jid"`
+	Uid         string      `json:"uid"`
+	Audiorecord bool        `json:"audiorecord,omitempty"`
 }
 
 type ClientCallSound struct {
-	Jid    tdproto.JID `json:"jid"`
-	Muted bool `json:"muted"`
+	Jid   tdproto.JID `json:"jid"`
+	Muted bool        `json:"muted"`
 }
 
 type ClientCallMuteAll struct {
-	Jid    tdproto.JID `json:"jid"`
+	Jid tdproto.JID `json:"jid"`
 }
 
 type ClientCallReject struct {
@@ -31,7 +30,7 @@ type ClientCallReject struct {
 }
 
 type ClientCallLeave struct {
-	Jid    tdproto.JID `json:"jid"`
-	Reason string      `json:"reason"`
-	LeaveWithoutClosing bool   `json:"test_not_in_room,omitempty"`
+	Jid                 tdproto.JID `json:"jid"`
+	Reason              string      `json:"reason"`
+	LeaveWithoutClosing bool        `json:"test_not_in_room,omitempty"`
 }
