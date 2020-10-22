27ef53f1544fbe333f14edeed2a58c983a17ef21
Victor <neznaikin0000@mail.ru>
2020-10-22 12:48:01 +0400

    fixing tag

--- features.go
+++ features.go
@@ -68,11 +68,11 @@ type Features struct {
 	// Maximum length for ColorRule description
 	MaxColorRuleDescriptionLength int `json:"max_color_rule_description_length"`
 
-	// Maximum length for Integration comment
-	MaxIntegrationCommentLength int `json:"max_integration_comment_length"`
-
 	//Maximum length for urls
 	MaxUrlLength int `json:"max_url_length"`
+
+	// Maximum length for Integration comment
+	MaxIntegrationCommentLength int `json:"max_integration_comment_length"`
 
 	// Maximum teams for one account
 	MaxTeams int `json:"max_teams"`
