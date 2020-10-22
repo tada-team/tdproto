520e70be3236aad8ea4657a35ecf1100aed18c52
Maxim Oransky <maxim.oransky@gmail.com>
2020-10-22 17:26:15 +0300

    AsteriskMention

--- user.go
+++ user.go
@@ -1,16 +1,39 @@
 package tdproto
 
+// Account data
 type User struct {
-	Phone            *string `json:"phone"`
-	Email            *string `json:"email"`
-	DefaultLang      *string `json:"default_lang"`
-	AltSend          bool    `json:"alt_send"`
-	AlwaysSendPushes bool    `json:"always_send_pushes"`
-	UnreadFirst      bool    `json:"unread_first"`
-	MUnreadFirst     bool    `json:"munread_first"`
-	Timezone         string  `json:"timezone"`
-	QuietTimeStart   *string `json:"quiet_time_start"`
-	QuietTimeFinish  *string `json:"quiet_time_finish"`
+	// Phone for login
+	Phone *string `json:"phone"`
+
+	// Email for login
+	Email *string `json:"email"`
+
+	// Default language code
+	DefaultLang *string `json:"default_lang"`
+
+	// Use Ctrl/Cmd + Enter instead Enter
+	AltSend bool `json:"alt_send"`
+
+	// Use * as @ for mentions
+	AsteriskMention bool `json:"asterisk_mention"`
+
+	// Send pushes even user is online
+	AlwaysSendPushes bool `json:"always_send_pushes"`
+
+	// Show unread chats in chat list first
+	UnreadFirst bool `json:"unread_first"`
+
+	// Show unread chats in chat list first on mobiles
+	MUnreadFirst bool `json:"munread_first"`
+
+	// Timezone
+	Timezone string `json:"timezone"`
+
+	// Start silently time (no pushes, no sounds)
+	QuietTimeStart *string `json:"quiet_time_start"`
+
+	// Finish silently time (no pushes, no sounds)
+	QuietTimeFinish *string `json:"quiet_time_finish"`
 }
 
 type UserWithMe struct {
