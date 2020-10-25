3bef032a06896bb91f16d3d0ae2a9e4ee8abd16b
Maxim Oransky <maxim.oransky@gmail.com>
2020-10-25 23:59:53 +0300

    Session

---
+++ session.go
@@ -0,0 +1,28 @@
+package tdproto
+
+// Websocket session
+type Session struct {
+	// Session id
+	Uid string `json:"uid"`
+
+	// Creation datetime
+	Created string `json:"created"`
+
+	// Language code
+	Lang string `json:"lang,omitempty"`
+
+	// Team id
+	Team string `json:"team,omitempty"`
+
+	// Mobile
+	IsMobile bool `json:"is_mobile,omitempty"`
+
+	// Away from keyboard
+	Afk bool `json:"afk,omitempty"`
+
+	// User agent
+	Useragent string `json:"useragent,omitempty"`
+
+	// IP address
+	Addr string `json:"addr,omitempty"`
+}
