cc74f408d6e9f7fb60fe050709d22e2b9e1cf339
Maxim Oransky <maxim.oransky@gmail.com>
2020-11-04 15:04:07 +0300

    group forms

---
+++ tdapi/group.go
@@ -0,0 +1,43 @@
+package tdapi
+
+import "github.com/tada-team/tdproto"
+
+type GroupMember struct {
+	// Member jid
+	Jid tdproto.JID `json:"jid"`
+
+	// Status: member or admin
+	Status string `json:"status,omitempty"`
+}
+
+type Group struct {
+	// Group title
+	DisplayName string `json:"display_name"`
+
+	// Group description, optiopnal
+	Description string `json:"description"`
+
+	// Readonly for non-admins group chat (Like Channels in Telegram bug switchable)
+	ReadonlyForMembers bool `json:"readonly_for_members"`
+
+	// Group deleted
+	IsArchive bool `json:"is_archive"`
+
+	// Can other team member see this group
+	Public bool `json:"public"`
+
+	// Any new team member will be added to this group chat
+	DefaultForAll bool `json:"default_for_all"`
+
+	// Delete messages in this chat in seconds. Experemental function
+	AutocleanupAge *int `json:"autocleanup_age"`
+
+	// Pinned message for this group
+	PinnedMessage string `json:"pinned_message"`
+
+	// Group members list
+	Members []GroupMember `json:"members"`
+
+	// Deprecated
+	Section string `json:"section"`
+}
