87446f1440cb0a29d6b0b7967f6d7d04590838d8
Maxim Oransky <maxim.oransky@gmail.com>
2020-11-27 05:08:51 +0300

    imports fix

--- inspect/make-dart.sh
+++ inspect/make-dart.sh
@@ -6,7 +6,7 @@ dest=../../tdproto_dart/lib/src
 go run dart/main.go $dest
 
 cd $dest
-dartfmt --line-length 120 -w .
 flutter pub get
 flutter packages pub run build_runner build --delete-conflicting-outputs
+dartfmt -l 120 -w .
 
