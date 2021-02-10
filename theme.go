33f884ea9438209c6e86a9c04ac85bacb136a75d
Ivan Palage4a <vvnvvplg@gmail.com>
2021-02-10 09:26:35 +0300

    fix: bad json syntax

--- theme.go
+++ theme.go
@@ -1,23 +1,23 @@
 package tdproto
 
 type ButtonColors struct {
-	BrandStatic   string `json:brand_static`
-	BrandActive   string `json:brand_active`
-	BrandDisable  string `json:brand_disable`
-	SimpleStatic  string `json:simple_static`
-	SimpleActive  string `json:simple_active`
-	SimpleDisable string `json:simple_disable`
+	BrandStatic   string `json:"brand_static"`
+	BrandActive   string `json:"brand_active"`
+	BrandDisable  string `json:"brand_disable"`
+	SimpleStatic  string `json:"simple_static"`
+	SimpleActive  string `json:"simple_active"`
+	SimpleDisable string `json:"simple_disable"`
 }
 
 type InputColors struct {
-	Static  string `json:static`
-	Active  string `json:active`
-	Disable string `json:disable`
-	Error   string `json:error`
+	Static  string `json:"static"`
+	Active  string `json:"active"`
+	Disable string `json:"disable"`
+	Error   string `json:"error"`
 }
 type SwitcherColors struct {
-	On  string `json:on`
-	Off string `json:off`
+	On  string `json:"on"`
+	Off string `json:"off"`
 }
 
 // Color theme
@@ -39,22 +39,22 @@ type Theme struct {
 	AppAccentColor  string
 	AppPrimaryColor string
 
-	Brand          string          `json:brand`
-	BrandDark      string          `json:brand_dark`
-	BrandLight     string          `json:brand_light`
-	Text           string          `json:text`
-	Sub            string          `json:sub`
-	Back           string          `json:back`
-	BackLight      string          `json:back_light`
-	BackDark       string          `json:back_dark`
-	Error          string          `json:error`
-	ErrorLight     string          `json:error_light`
-	Success        string          `json:success`
-	SuccessLight   string          `json:success_light`
-	Background     string          `json:background`
-	Attention      string          `json:attention`
-	AttentionLight string          `json:attention_light`
-	Button         *ButtonColors   `json:button`
-	Input          *InputColors    `json:input`
-	Switcher       *SwitcherColors `json:switcher`
+	Brand          string          `json:"brand"`
+	BrandDark      string          `json:"brand_dark"`
+	BrandLight     string          `json:"brand_light"`
+	Text           string          `json:"text"`
+	Sub            string          `json:"sub"`
+	Back           string          `json:"back"`
+	BackLight      string          `json:"back_light"`
+	BackDark       string          `json:"back_dark"`
+	Error          string          `json:"error"`
+	ErrorLight     string          `json:"error_light"`
+	Success        string          `json:"success"`
+	SuccessLight   string          `json:"success_light"`
+	Background     string          `json:"background"`
+	Attention      string          `json:"attention"`
+	AttentionLight string          `json:"attention_light"`
+	Button         *ButtonColors   `json:"button"`
+	Input          *InputColors    `json:"input"`
+	Switcher       *SwitcherColors `json:"switcher"`
 }
