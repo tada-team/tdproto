8ea3d315a7e36cef0818b24f565f3f96ddfcc45e
Ivan Palage4a <vvnvvplg@gmail.com>
2021-02-10 06:50:10 +0300

    new fields for app's colors

--- theme.go
+++ theme.go
@@ -1,7 +1,28 @@
 package tdproto
 
+type ButtonColors struct {
+	BrandStatic   string `json:brand_static`
+	BrandActive   string `json:brand_active`
+	BrandDisable  string `json:brand_disable`
+	SimpleStatic  string `json:simple_static`
+	SimpleActive  string `json:simple_active`
+	SimpleDisable string `json:simple_disable`
+}
+
+type InputColors struct {
+	Static  string `json:static`
+	Active  string `json:active`
+	Disable string `json:disable`
+	Error   string `json:error`
+}
+type SwitcherColors struct {
+	On  string `json:on`
+	Off string `json:off`
+}
+
 // Color theme
 type Theme struct {
+	// Web colors
 	BgColor                string
 	BgHoverColor           string
 	TextColor              string
@@ -13,6 +34,27 @@ type Theme struct {
 	MainAccentHover        string
 	MainLightAccent        string
 	MainLink               string
-	AppAccentColor         string
-	AppPrimaryColor        string
+
+	// App colors
+	AppAccentColor  string
+	AppPrimaryColor string
+
+	Brand          string `json:brand`
+	BrandDark      string `json:brand_dark`
+	BrandLight     string `json:brand_light`
+	Text           string `json:text`
+	Sub            string `json:sub`
+	Back           string `json:back`
+	BackLight      string `json:back_light`
+	BackDark       string `json:back_dark`
+	Error          string `json:error`
+	ErrorLight     string `json:error_light`
+	Success        string `json:success`
+	SuccessLight   string `json:success_light`
+	Backgroud      string `json:background`
+	Attention      string `json:attention`
+	AttentionLight string `json:attentionLight`
+	Button         ButtonColors
+	Input          InputColors
+	Switcher       SwitcherColors
 }
