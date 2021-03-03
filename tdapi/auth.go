de1a5f37860ca34762e1944e61b78f0078e1d5ca
E.Kalinin <shoreward@gmail.com>
2021-03-03 13:22:22 +0300

    2fa email recovery fields

--- tdapi/auth.go
+++ tdapi/auth.go
@@ -8,28 +8,88 @@ type Auth struct {
 	Required2fa bool               `json:"required2fa"`
 }
 
-type Auth2faForm struct {
-	Token    string `json:"token"`
+type Auth2faSettingsResponse struct {
+	Enabled        bool   `json:"enabled"`
+	RecoveryStatus string `json:"recovery_status"`
+}
+
+type Auth2faMailRecovery struct {
+	CodeValidUntil string `json:"code_valid_until"`
+	NextCodeAt     string `json:"next_code_at"`
+	CodeLength     int    `json:"code_length"`
+}
+
+type Auth2faSettingsMailValidation struct {
+	Enabled        bool   `json:"enabled"`
+	RecoveryStatus string `json:"recovery_status"`
+
+	CodeValidUntil string `json:"code_valid_until"`
+	NextCodeAt     string `json:"next_code_at"`
+	CodeLength     int    `json:"code_length"`
+}
+
+type basicPasswordUpdate struct {
+	NewPassword       string `json:"new_password"`
+	NewPasswordRepeat string `json:"new_password_repeat"`
+	Hint              string `json:"hint"`
+}
+
+type basicToken struct {
+	Token string `json:"token"`
+}
+
+type basicPassword struct {
 	Password string `json:"password"`
 }
 
-type Auth2faSettingsResponse struct {
-	Enabled bool `json:"enabled"`
+type basicEmail struct {
+	Email string `json:"email"`
 }
 
-type Auth2faSettingsSetForm struct {
-	NewPassword       string `json:"new_password"`
-	NewPasswordRepeat string `json:"new_password_repeat"`
-	Hint              string `json:"hint"`
+type basicCode struct {
+	Code string `json:"code"`
 }
 
-type Auth2faSettingsUpdateForm struct {
-	OldPassword       string `json:"old_password"`
-	NewPassword       string `json:"new_password"`
-	NewPasswordRepeat string `json:"new_password_repeat"`
-	Hint              string `json:"hint"`
+type Auth2faForm struct {
+	basicToken
+	basicPassword
 }
 
-type Auth2faSettingsDeleteForm struct {
-	OldPassword string `json:"old_password"`
+type Create2faPasswordForm struct {
+	basicPasswordUpdate
+}
+
+type Update2faPasswordForm struct {
+	basicPasswordUpdate
+	basicPassword
+}
+
+type Internal2faPasswordForm struct {
+	basicPassword
+}
+
+type SendMail2faConfirmForm struct {
+	basicPassword
+	basicEmail
+}
+
+type ConfirmMail2faForm struct {
+	basicPassword
+	basicCode
+	basicEmail
+}
+
+type AuthToken2faForm struct {
+	basicToken
+}
+
+type AuthCheckCode2faForm struct {
+	basicToken
+	basicCode
+}
+
+type AuthPasswordRecovery2faForm struct {
+	basicPasswordUpdate
+	basicToken
+	basicCode
 }
