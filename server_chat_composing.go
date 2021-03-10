78f40c8b50a28bed8f1b78d606a85a05244f04a7
Maxim Oransky <maxim.oransky@gmail.com>
2021-03-10 05:55:05 +0300

    ValidUntil type fix

--- server_chat_composing.go
+++ server_chat_composing.go
@@ -1,7 +1,5 @@
 package tdproto
 
-import "time"
-
 func NewServerChatComposing(composing, isAudio bool, chat, actor *JID) (r ServerChatComposing) {
 	r.Name = r.GetName()
 	r.Params.Jid = *chat
@@ -19,9 +17,9 @@ type ServerChatComposing struct {
 func (p ServerChatComposing) GetName() string { return "server.chat.composing" }
 
 type serverChatComposingParams struct {
-	Jid        JID       `json:"jid"`
-	Actor      JID       `json:"actor"`
-	Composing  bool      `json:"composing"`
-	IsAudio    bool      `json:"is_audio,omitempty"`
-	ValidUntil time.Time `json:"valid_until,omitempty"`
+	Jid        JID               `json:"jid"`
+	Actor      JID               `json:"actor"`
+	Composing  bool              `json:"composing"`
+	IsAudio    bool              `json:"is_audio,omitempty"`
+	ValidUntil ISODateTimeString `json:"valid_until,omitempty"`
 }
