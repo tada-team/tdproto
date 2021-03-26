8c37e113b11c20dc195c0b02cea10e00243fa73d
igo95862 <igo95862@yandex.ru>
2021-03-26 17:08:24 +0300

    Initial refactored version of parser
    Missing support for inheritance
    Missing maps
    Missing Interfaces
    Missing several possible types:
    
    * Selector
    * Interface
    * Pointer to array of interfaces
    * Pointer to selector
    * Pointer to maps

--- codegen/inspect.go
+++ codegen/inspect.go
@@ -1,15 +1,13 @@
 package codegen
 
 import (
-	"bytes"
 	"fmt"
 	"go/ast"
 	"go/parser"
-	"go/printer"
 	"go/token"
 	"io"
+	"os"
 	"reflect"
-	"sort"
 	"strings"
 	"text/template"
 
@@ -17,51 +15,36 @@ import (
 	"github.com/tada-team/tdproto"
 )
 
-type EnumValue struct {
+type TadaConstFields struct {
 	Name  string `json:"name"`
+	Type  string
 	Value string `json:"value"`
 	Help  string `json:"help"`
 }
 
-func (e EnumValue) DartValue() string { return strings.ReplaceAll(e.Value, `"`, `'`) }
-
-func (e EnumValue) DartName() string { return strcase.ToLowerCamel(e.Name) }
-
-type TadaField struct {
-	Name         string `json:"name"`
-	JSName       string `json:"js_name"`
-	Help         string `json:"help"`
-	Json         string `json:"json"`
-	Type         string `json:"type"`
-	Readonly     bool   `json:"readonly"`
-	Null         bool   `json:"null,omitempty"`
-	List         bool   `json:"list,omitempty"`
-	InternalType bool   `json:"internal_type,omitempty"`
-	Omitempty    bool   `json:"omitempty,omitempty"`
-	TSType       string `json:"ts_type"`
-	DartType     string `json:"dart_type"`
-	TSDefault    string `json:"ts_default"`
-}
-
-func (f TadaField) DartRequired() bool {
-	return !(f.Null || f.Omitempty)
-}
-
-func (f TadaField) DartName() string {
-	switch f.JSName {
-	case "default":
-		return "isDefault"
-	default:
-		return f.JSName
-	}
+type TadaStructField struct {
+	Name        string
+	Help        string
+	JsonName    string
+	TypeStr     string
+	IsReadOnly  bool
+	IsPointer   bool
+	IsList      bool
+	IsOmitEmpty bool
 }
 
 type TadaStruct struct {
-	Name       string       `json:"name"`
-	Help       string       `json:"help"`
-	Fields     []*TadaField `json:"fields"`
-	Readonly   bool         `json:"readonly,omitempty"`
-	EnumValues []EnumValue  `json:"enum_values,omitempty"`
+	Name       string             `json:"name"`
+	Help       string             `json:"help"`
+	Fields     []*TadaStructField `json:"fields"`
+	ReadOnly   bool               `json:"readonly,omitempty"`
+	EnumValues []TadaConstFields  `json:"enum_values,omitempty"`
+}
+
+type TadaType struct {
+	Name     string `json:"name"`
+	Help     string `json:"help"`
+	BaseType string
 }
 
 type TadaEvent struct {
@@ -75,7 +58,9 @@ func (s TadaStruct) IsEnum() bool { return len(s.EnumValues) > 0 }
 
 type TadaInfo struct {
 	TadaStructs []TadaStruct
+	TadaTypes   []TadaType
 	Events      []TadaEvent
+	TadaConsts  []TadaConstFields
 }
 
 func Render(wr io.Writer, s string) error {
@@ -84,7 +69,7 @@ func Render(wr io.Writer, s string) error {
 		return err
 	}
 
-	p, err := ParseTdProto()
+	p, err := ParseTdproto()
 	if err != nil {
 		return err
 	}
@@ -92,201 +77,288 @@ func Render(wr io.Writer, s string) error {
 	return tpl.Execute(wr, p)
 }
 
-func ParseTdProto() (p TadaInfo, err error) {
-	fset := token.NewFileSet()
-	enumsMap := make(map[string][]EnumValue)
-	if err := extractTdprotoAst(fset, token.CONST, func(gen *ast.GenDecl, eventNames map[string]*ast.FuncDecl) error {
-		for _, spec := range gen.Specs {
-			valueSpec, ok := spec.(*ast.ValueSpec)
-			if !ok || valueSpec.Type == nil {
-				continue
-			}
+func ParseTdproto() (infoToFill *TadaInfo, err error) {
+	tdprotoFileSet := token.NewFileSet()
 
-			typeName := fmt.Sprintf("%s", valueSpec.Type) // XXX:
-			if typeName == "" {
-				continue
-			}
+	infoToFill = new(TadaInfo)
 
-			name := valueSpec.Names[0]
-			val, ok := valueSpec.Values[0].(*ast.BasicLit)
-			if !ok {
-				continue
-			}
+	tdprotoNameToAstMap, err := extractTdprotoAst(tdprotoFileSet)
 
-			if enumsMap[typeName] == nil {
-				enumsMap[typeName] = []EnumValue{}
-			}
-
-			enumsMap[typeName] = append(enumsMap[typeName], EnumValue{
-				Name:  name.Name,
-				Value: val.Value,
-				Help:  cleanHelp(valueSpec.Doc.Text()),
-			})
-		}
-		return nil
-	}); err != nil {
-		return p, err
+	if err != nil {
+		return nil, err
 	}
 
-	types := make(map[string]struct{})
-	if err := extractTdprotoAst(fset, token.TYPE, func(gen *ast.GenDecl, eventNames map[string]*ast.FuncDecl) error {
-		s := TadaStruct{Help: cleanHelp(gen.Doc.Text())}
-		if s.Help == "" || strings.HasPrefix(strings.ToLower(s.Help), "deprecated") {
-			return nil
+	var tdprotoAst = tdprotoNameToAstMap["tdproto"]
+
+	for fileName, fileAst := range tdprotoAst.Files {
+		err = ParseTdprotoFile(infoToFill, fileName, fileAst)
+		if err != nil {
+			return infoToFill, err
+		}
+	}
+
+	return infoToFill, nil
+}
+
+func ParseTdprotoFile(infoToFill *TadaInfo, fileName string, fileAst *ast.File) error {
+
+	var err error = nil
+
+	for _, declaration := range fileAst.Decls {
+
+		switch declarationType := declaration.(type) {
+		case *ast.GenDecl:
+			err = ParseGenericDeclaration(infoToFill, declarationType)
+		}
+	}
+
+	return err
+}
+
+func ParseGenericDeclaration(infoToFill *TadaInfo, genDeclaration *ast.GenDecl) error {
+	var err error = nil
+	switch genDeclaration.Tok {
+	case token.CONST:
+		err = parseConstDeclaration(infoToFill, genDeclaration)
+	case token.TYPE:
+		err = parseTypeDeclaration(infoToFill, genDeclaration)
+	}
+
+	return err
+}
+
+func parseTypeDeclaration(infoToFill *TadaInfo, genDeclaration *ast.GenDecl) error {
+	// parse type Name struct|type {Field} declarations
+
+	numberOfSpecs := len(genDeclaration.Specs)
+
+	if numberOfSpecs != 1 {
+		return fmt.Errorf("unsupported number of specs %#v", genDeclaration)
+	}
+
+	helpString := cleanHelp(genDeclaration.Doc.Text())
+
+	declarationSpec := genDeclaration.Specs[0].(*ast.TypeSpec)
+
+	switch typeAst := declarationSpec.Type.(type) {
+	case *ast.Ident:
+		err := parseTypeDefinition(infoToFill, declarationSpec, typeAst)
+		if err != nil {
+			return err
+		}
+	case *ast.StructType:
+		err := parseStructDefinitioninfo(infoToFill, declarationSpec, typeAst, helpString)
+		if err != nil {
+			return err
+		}
+	default:
+		fmt.Fprintf(os.Stderr, "WARN: Not implemented type declaration %#v\n", typeAst)
+	}
+
+	return nil
+}
+
+func parseTypeDefinition(infoToFill *TadaInfo, declarationSpec *ast.TypeSpec, typeIndent *ast.Ident) error {
+
+	var typeName string = declarationSpec.Name.Name
+
+	var newTadaType = TadaType{
+		Name:     typeName,
+		BaseType: typeIndent.Name,
+	}
+
+	_ = append(infoToFill.TadaTypes, newTadaType)
+	return nil
+}
+
+func parseStructDefinitioninfo(infoToFill *TadaInfo, declarationSpec *ast.TypeSpec, structInfo *ast.StructType, helpString string) error {
+
+	if helpString == "" {
+		fmt.Fprintf(os.Stderr, "WARN: TadaStruct missing a doc string %+v\n", structInfo)
+	}
+
+	if strings.HasPrefix(strings.ToLower(helpString), "deprecated") {
+
+		return nil
+	}
+
+	structName := declarationSpec.Name.Name
+
+	isReadOnly := strings.Contains(helpString, "Readonly")
+
+	var fieldsList []TadaStructField
+
+	for _, field := range structInfo.Fields.List {
+		fieldNamesLength := len(field.Names)
+		if fieldNamesLength == 0 {
+			// TODO: implement inheritance
+			continue
+		} else if fieldNamesLength == 1 {
+		} else {
+			return fmt.Errorf("unexpected struct %s field name amount of %d", structName, len(field.Names))
 		}
 
-		s.Readonly = strings.Contains(s.Help, "Readonly") // XXX
-		for _, spec := range gen.Specs {
-			typeSpec, ok := spec.(*ast.TypeSpec)
-			if !ok {
-				continue
+		fieldName := field.Names[0].Name
+		isOmitEmpty := false
+		isReadOnly := false
+		jsonName := fieldName
+
+		if field.Tag != nil {
+			structTags := reflect.StructTag(strings.Trim(field.Tag.Value, "`"))
+
+			var jsonTags []string
+			jsonTagsStr, ok := structTags.Lookup("json")
+			if ok {
+				jsonTags = strings.Split(jsonTagsStr, ",")
 			}
 
-			s.Name = typeSpec.Name.Name
-			if eventNames[s.Name] != nil {
-				p.Events = append(p.Events, TadaEvent{
-					Name: strcase.ToDelimited(s.Name, '.'),
-					Help: s.Help,
-				})
-				continue
-			}
+			for i, aTag := range jsonTags {
+				if i == 0 {
+					if aTag == "-" {
+						continue
+					}
 
-			types[s.Name] = struct{}{}
-			s.EnumValues = enumsMap[s.Name]
-			if len(s.EnumValues) > 0 {
-				for i := range s.EnumValues {
-					s.EnumValues[i].Name = strings.TrimSuffix(s.EnumValues[i].Name, s.Name)
-				}
-			} else {
-				switch typeSpec.Type.(type) {
-				case *ast.StructType:
-
-					st := typeSpec.Type.(*ast.StructType)
-					for _, field := range st.Fields.List {
-						name := field.Names[0].Name
-
-						omitempty := false
-						readonly := false
-						jsonName := name
-						if field.Tag != nil {
-							tag := reflect.StructTag(strings.Trim(field.Tag.Value, "`"))
-							jsontag := strings.Split(tag.Get("json"), ",")
-
-							omitempty = len(jsontag) == 2 && jsontag[1] == "omitempty"
-
-							jsonName = jsontag[0]
-							if jsonName == "-" {
-								continue
-							}
-
-							for _, tag := range strings.Split(tag.Get("tdproto"), ",") {
-								if tag == "readonly" {
-									readonly = true
-									break
-								}
-							}
-						}
-
-						var typeNameBuf bytes.Buffer
-						if err := printer.Fprint(&typeNameBuf, fset, field.Type); err != nil {
-							return err
-						}
-
-						nullable := false
-						list := false
-
-						typeDescr := typeNameBuf.String()
-						if strings.HasPrefix(typeDescr, "*") {
-							nullable = true
-							typeDescr = typeDescr[1:]
-						}
-
-						if strings.HasPrefix(typeDescr, "[]") {
-							list = true
-							typeDescr = typeDescr[2:]
-						}
-
-						tsType := typesMap[typeDescr].TypeScript
-						if tsType == "" {
-							tsType = typeDescr
-						}
-
-						dartType := typesMap[typeDescr].Dart
-						if dartType == "" {
-							dartType = typeDescr
-						}
-
-						if nullable && !omitempty && typeDescr == "JID" { // XXX:
-							nullable = false
-						}
-
-						help := cleanHelp(field.Doc.Text())
-						if help == "" {
-							help = name
-						}
-
-						tsDefault := ""
-						if omitempty {
-							if nullable {
-								tsDefault = "null"
-							} else {
-								tsDefault = tsDefaultMap[tsType]
-								if tsDefault == "" {
-									tsDefault = "unknown: " + tsType
-								}
-							}
-						}
-
-						s.Fields = append(s.Fields, &TadaField{
-							Name:      name,
-							Help:      help,
-							JSName:    strings.ToLower(name[:1]) + name[1:],
-							TSType:    tsType,
-							TSDefault: tsDefault,
-							DartType:  dartType,
-							Json:      jsonName,
-							Type:      typeDescr,
-							Null:      nullable,
-							List:      list,
-							Omitempty: omitempty,
-							Readonly:  readonly,
-						})
+					jsonName = aTag
+				} else {
+					if aTag == "omitempty" {
+						isOmitEmpty = true
+					} else {
+						return fmt.Errorf("unknown json tag %s", aTag)
 					}
 				}
 			}
 
-			p.TadaStructs = append(p.TadaStructs, s)
+			var tdprotoTags []string
+			tdprotoTagsStr, ok := structTags.Lookup("tdproto")
+			if ok {
+				tdprotoTags = strings.Split(tdprotoTagsStr, ",")
+			}
+
+			for _, aTag := range tdprotoTags {
+				if aTag == "readonly" {
+					isReadOnly = true
+				} else {
+					return fmt.Errorf("unknown tdproto tag %s", aTag)
+				}
+			}
 		}
-		return nil
-	}); err != nil {
-		return p, err
+
+		isList := false
+		isPointer := false
+		var fieldTypeStr string
+
+		switch fieldTypeAst := field.Type.(type) {
+		case *ast.Ident:
+			fieldTypeStr = fieldTypeAst.Name
+		case *ast.ArrayType:
+			isList = true
+			arrayExprAst, ok := fieldTypeAst.Elt.(*ast.Ident)
+			if !ok {
+				// TODO: Implement pointers to array of interfaces
+				continue
+			}
+
+			fieldTypeStr = arrayExprAst.Name
+		case *ast.StarExpr:
+			isPointer = true
+
+			switch pointedType := fieldTypeAst.X.(type) {
+			case *ast.Ident:
+				fieldTypeStr = pointedType.Name
+			case *ast.ArrayType:
+				isList = true
+
+				arrayExprAst := pointedType.Elt.(*ast.Ident)
+
+				fieldTypeStr = arrayExprAst.Name
+			case *ast.MapType:
+				// TODO: Implement pointers to maps
+			case *ast.SelectorExpr:
+				// TODO: Implement pointers to selectors
+			default:
+				panic(fmt.Sprintf("Unknown pointer field of %s type %#v", structName, pointedType))
+			}
+
+		case *ast.SelectorExpr:
+			// TODO: Implement selector expresion. Example time.Time
+			continue
+		case *ast.InterfaceType:
+			// TODO: Implement interface expression.
+			continue
+		default:
+			return fmt.Errorf("unknown field of %s type %#v", structName, fieldTypeAst)
+		}
+
+		if fieldTypeStr == "" {
+			return fmt.Errorf("empty field name %s of %s", structName, fieldName)
+
+		}
+
+		_ = append(fieldsList, TadaStructField{
+			Name:        fieldName,
+			IsReadOnly:  isReadOnly,
+			IsOmitEmpty: isOmitEmpty,
+			JsonName:    jsonName,
+			TypeStr:     fieldTypeStr,
+			IsList:      isList,
+			IsPointer:   isPointer,
+		})
 	}
 
-	sort.Slice(p.TadaStructs, func(i, j int) bool {
-		return p.TadaStructs[i].Name < p.TadaStructs[j].Name
-	})
-
-	sort.Slice(p.Events, func(i, j int) bool {
-		return p.Events[i].Name < p.Events[j].Name
-	})
-
-	for _, s := range p.TadaStructs {
-		for _, f := range s.Fields {
-			_, f.InternalType = types[f.Type]
-		}
+	var newTadaStruct = TadaStruct{
+		Help:     helpString,
+		ReadOnly: isReadOnly,
+		Name:     structName,
 	}
 
-	return p, nil
+	_ = append(infoToFill.TadaStructs, newTadaStruct)
+	return nil
 }
 
-func extractTdprotoAst(fset *token.FileSet, tok token.Token, fn func(gen *ast.GenDecl, eventNames map[string]*ast.FuncDecl) error) error {
-	tdProtoPath := tdproto.SourceDir()
-	_, err := parser.ParseDir(fset, tdProtoPath, nil, parser.ParseComments)
-	if err != nil {
-		return err
+func parseConstDeclaration(infoToFill *TadaInfo, genDeclaration *ast.GenDecl) error {
+	// Parse const ( name Type = value ...) expressions
+
+	for _, spec := range genDeclaration.Specs {
+		valueSpec, ok := spec.(*ast.ValueSpec)
+		if !ok {
+			return fmt.Errorf("expected const spec got %+v", spec)
+		}
+
+		if len(valueSpec.Names) != 1 {
+			return fmt.Errorf("expected one constant name got %+v", valueSpec.Names)
+		}
+
+		constName := valueSpec.Names[0].Name
+
+		constTypeName := fmt.Sprintf("%s", valueSpec.Type)
+		if constTypeName == "" {
+			fmt.Printf("WARN: const has no typeName %s\n", constName)
+			continue
+		}
+
+		if len(valueSpec.Values) != 1 {
+			return fmt.Errorf("expected one constant value got %+v", valueSpec.Values)
+		}
+
+		constValue, ok := valueSpec.Values[0].(*ast.BasicLit)
+		if !ok {
+			return fmt.Errorf("could not extract constant value %+v", valueSpec.Values[0])
+		}
+
+		_ = append(infoToFill.TadaConsts, TadaConstFields{
+			Name:  constName,
+			Type:  constTypeName,
+			Value: constValue.Value,
+			Help:  cleanHelp(valueSpec.Doc.Text()),
+		})
 	}
 
 	return nil
+}
+
+func extractTdprotoAst(fileSet *token.FileSet) (map[string]*ast.Package, error) {
+	tdProtoPath := tdproto.SourceDir()
+	return parser.ParseDir(fileSet, tdProtoPath, nil, parser.ParseComments)
 }
 
 func cleanHelp(s string) string {
