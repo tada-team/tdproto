abc939bec0b0bb237dad6edd0856ad4cc60eb792
igo95862 <igo95862@yandex.ru>
2021-03-31 16:51:52 +0300

    dart: Started with a stub

--- codegen/dart/main.go
+++ codegen/dart/main.go
@@ -1,5 +1,26 @@
 package main
 
-func main() {
+import (
+	"github.com/tada-team/tdproto/codegen"
+)
 
+const dartHeader = `
+import 'package:freezed_annotation/freezed_annotation.dart';`
+
+const dartClassTemplate = `
+
+`
+
+func generateDart(tdprotoInfo *codegen.TdInfo) {
+
+}
+
+func main() {
+	tdprotoInfo, err := codegen.ParseTdproto()
+
+	if err != nil {
+		panic(err)
+	}
+
+	generateDart(tdprotoInfo)
 }
