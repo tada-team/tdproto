67cd43e4561d7b292062553790b541d0b36867d5
Maxim Oransky <maxim.oransky@gmail.com>
2021-04-17 01:18:42 +0300

    TeamShort response

--- tdapi/api_v4_join_token_preview.go
+++ tdapi/api_v4_join_token_preview.go
@@ -7,22 +7,48 @@ import (
 
 func init() {
 	register("/api/v4/join/[token]/preview", openapi.Path{
-		Parameters: []openapi.Parameter{
-			{
-				Name:        "token",
-				In:          "path",
-				Required:    true,
-				Description: "Invitation token",
-				Schema: openapi.Schema{
-					Type: openapi.String,
-				},
+		Parameters: []openapi.Parameter{{
+			Name:        "token",
+			In:          "path",
+			Required:    true,
+			Description: "Invitation token",
+			Schema: openapi.Schema{
+				Type: openapi.String,
 			},
-		},
+		}},
 		Get: &openapi.Operation{
 			Summary: "Invitation information",
 			Responses: openapi.Responses{
 				Status200: okResponse(openapi.Property{
-					Type: openapi.String,
+					Type: openapi.Object,
+					Properties: map[string]openapi.Property{
+						"uid": {
+							Type:        openapi.String,
+							Description: "Team id",
+						},
+						"name": {
+							Type:        openapi.String,
+							Description: "Team name",
+						},
+						"icons": {
+							Type:        openapi.String,
+							Description: "Team icons",
+							Properties: map[string]openapi.Property{
+								"stub": {
+									Type:        openapi.String,
+									Description: "Generated image with 1-2 letters",
+								},
+								"letters": {
+									Type:        openapi.String,
+									Description: "Letters from stub icon",
+								},
+								"color": {
+									Type:        openapi.String,
+									Description: "Stub icon background color",
+								},
+							},
+						},
+					},
 					Example: tdproto.TeamShort{
 						Uid:  "123e4567-e89b-12d3-a456-426614174000",
 						Name: "Test Team",
--- tdapi/openapi/property.go
+++ tdapi/openapi/property.go
@@ -1,9 +1,10 @@
 package openapi
 
 type Property struct {
-	Type        Type        `json:"type,omitempty"`
-	Format      Format      `json:"format,omitempty"`
-	Description string      `json:"description,omitempty"`
-	Example     interface{} `json:"example,omitempty"`
+	Type        Type                `json:"type,omitempty"`
+	Format      Format              `json:"format,omitempty"`
+	Description string              `json:"description,omitempty"`
+	Example     interface{}         `json:"example,omitempty"`
+	Ref         string              `json:"$ref,omitempty"`
+	Properties  map[string]Property `json:"properties,omitempty"`
 }
-
