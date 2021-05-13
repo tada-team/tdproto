0fccbb2b60721c411eac28134b4316167faeb1cb
Maxim Oransky <maxim.oransky@gmail.com>
2021-05-13 13:50:14 +0300

    TaskColor docs

--- task_colors.go
+++ task_colors.go
@@ -2,7 +2,12 @@ package tdproto
 
 // Task color rules color
 type TaskColor struct {
+	// Regular color (HEX)
 	Regular string `json:"regular"`
+
+	// Dark color (HEX)
 	Dark    string `json:"dark"`
+
+	// Light color (HEX)
 	Light   string `json:"light"`
 }
