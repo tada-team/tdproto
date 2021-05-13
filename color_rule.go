fb13df239b263ce18110c3d619c3223e7690b9d5
Maxim Oransky <maxim.oransky@gmail.com>
2021-05-13 14:01:14 +0300

    ColorRule description

--- color_rule.go
+++ color_rule.go
@@ -2,17 +2,42 @@ package tdproto
 
 // Set of rules to apply to tasks for coloring.
 type ColorRule struct {
-	Uid                   string   `json:"uid"`
-	Priority              int      `json:"priority"`
-	ColorIndex            uint16   `json:"color_index"`
-	Section               string   `json:"section,omitempty"`
-	Tags                  []string `json:"tags,omitempty"`
-	Description           string   `json:"description,omitempty"`
-	TaskStatus            string   `json:"task_status,omitempty"`
-	TaskImportance        *int     `json:"task_importance,omitempty"`
-	TaskUrgency           *int     `json:"task_urgency,omitempty"`
-	SectionEnabled        *bool    `json:"section_enabled,omitempty"`
-	TaskImportanceEnabled *bool    `json:"task_importance_enabled,omitempty"`
-	TaskUrgencyEnabled    *bool    `json:"task_urgency_enabled,omitempty"`
-	TagsEnabled           *bool    `json:"tags_enabled,omitempty"`
+	// Rule id
+	Uid string `json:"uid"`
+
+	// Rule priority
+	Priority int `json:"priority"`
+
+	// Rule description
+	Description string `json:"description,omitempty"`
+
+	// Color index
+	ColorIndex uint16 `json:"color_index"`
+
+	// Project filter enabled
+	SectionEnabled *bool `json:"section_enabled,omitempty"`
+
+	// Project id if project filter enabled
+	Section string `json:"section,omitempty"`
+
+	// Tags filter enabled
+	TagsEnabled *bool `json:"tags_enabled,omitempty"`
+
+	// Tag ids if tags filter enabled
+	Tags []string `json:"tags,omitempty"`
+
+	// Task status
+	TaskStatus string `json:"task_status,omitempty"`
+
+	// Task importance filter enabled
+	TaskImportanceEnabled *bool `json:"task_importance_enabled,omitempty"`
+
+	// Task importance if task importance filter enabled
+	TaskImportance *int `json:"task_importance,omitempty"`
+
+	// Task urgency filter enabled
+	TaskUrgencyEnabled *bool `json:"task_urgency_enabled,omitempty"`
+
+	// Task urgency if task urgency filter enabled
+	TaskUrgency *int `json:"task_urgency,omitempty"`
 }
