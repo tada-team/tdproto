7034058b740075642be147bf9f7da429bc4d4dad
igo95862 <igo95862@yandex.ru>
2021-06-02 15:26:14 +0300

    api_paths: Added rest of misc paths.

--- codegen/api_paths/misc.go
+++ codegen/api_paths/misc.go
@@ -1,5 +1,7 @@
 package api_paths
 
+import "github.com/tada-team/tdproto"
+
 var MiscPaths = []PathSpec{
 	{
 		Path: "/api/v4/addr",
@@ -7,6 +9,21 @@ var MiscPaths = []PathSpec{
 			Responce:            "",
 			Description:         "Returns client address for debuging purposes.",
 			ResponceDescription: "Address of the server.",
+		},
+	},
+	{
+		Path: "/api/v4/ping",
+		Get: &HttpSpec{
+			Responce:            "",
+			Description:         "Ping the server.",
+			ResponceDescription: "Set to ``\"pong\"``.",
+		},
+	},
+	{
+		Path: "/features.json",
+		Get: &HttpSpec{
+			Responce:    tdproto.Features{},
+			Description: "Get the server features information.",
 		},
 	},
 }
