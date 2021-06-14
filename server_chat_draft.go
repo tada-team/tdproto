c0b53465889d6fa4745a709586bb4073318accea
Maxim Oransky <maxim.oransky@gmail.com>
2021-06-14 03:50:01 +0300

    server.chat.draft, changelog

--- server_chat_draft.go
+++ server_chat_draft.go
@@ -1,14 +1,14 @@
 package tdproto
 
-func NewServerChatDraft(jid JID, draft string, draftNum int64) (r ServerChatDraft) {
+func NewServerChatDraft(jid JID, draft string, gentime int64) (r ServerChatDraft) {
 	r.Name = r.GetName()
 	r.Params.Jid = jid
 	r.Params.Draft = draft
-	r.Params.DraftNum = draftNum
+	r.Params.Gentime = gentime
 	return r
 }
 
-// Changed draft message in chan
+// Changed draft message in chat
 type ServerChatDraft struct {
 	BaseEvent
 	Params serverChatDraftParams `json:"params"`
@@ -20,9 +20,12 @@ type serverChatDraftParams struct {
 	// Chat or contact id
 	Jid JID `json:"jid"`
 
+	// Draft version
+	Gentime int64 `json:"gentime" tdproto:"readonly"`
+
 	// Draft text
 	Draft string `json:"draft"`
 
-	// Draft version. TODO: use gentime instead
+	// Deprecated
 	DraftNum int64 `json:"draft_num"`
 }
