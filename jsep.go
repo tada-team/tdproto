7ef36851427ed46a247a8dc9f5d339ae0ab814a9
igo95862 <igo95862@yandex.ru>
2021-06-22 13:33:58 +0300

    doc: Document JSEP structure

--- jsep.go
+++ jsep.go
@@ -1,6 +1,9 @@
 package tdproto
 
+// JavaScript Session Establishment Protocol
 type JSEP struct {
-	SDP  string `json:"sdp"`
+	// Session Description Protocol information
+	SDP string `json:"sdp"`
+	// See https://rtcweb-wg.github.io/jsep/#rfc.section.4.1.8
 	Type string `json:"type"`
 }
