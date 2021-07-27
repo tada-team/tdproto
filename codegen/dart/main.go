b4741d2d1fdc9083a38b31718fdfb805999c9280
igo95862 <igo95862@yandex.ru>
2021-07-27 14:35:38 +0300

    dart: Dart classes can now be generated

--- codegen/dart/main.go
+++ codegen/dart/main.go
@@ -7,27 +7,108 @@ import (
 	"github.com/tada-team/tdproto/codegen"
 )
 
-const dartHeader = `
-import 'package:freezed_annotation/freezed_annotation.dart';`
+var dartTypeMap = map[string]string{
+	"string":            "String",
+	"int":               "int",
+	"int64":             "int",
+	"uint16":            "int",
+	"uint":              "int",
+	"ISODateTimeString": "DateTime",
+}
 
-const dartClassTemplate = `
+var dartClassTemplate = template.Must(template.New("dartClass").Parse(`import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:tdproto_dart/tdproto_dart.dart';
 
-`
-const dartEnumHeader = `import 'package:freezed_annotation/freezed_annotation.dart';
-`
+part '{{.Parent.Name}}.freezed.dart';
+part '{{.Parent.Name}}.g.dart';
 
-var dartEnumTemplate = template.Must(template.New("dartEnum").Parse(`
-enum {{.Name}} { {{ range $value :=  .Values}}
+/// {{.Parent.Help}}
+@freezed
+abstract class {{.Parent.Name}} with _${{.Parent.Name}} {
+  const factory {{.Parent.Name}}({
+    {{range $field := .Fields -}}
+    /// {{$field.Parent.Help}}
+    @JsonKey(name: '{{$field.Parent.JsonName}}') 
+	{{- if eq $field.DartType "DateTime"}} @DateTimeConverter(){{end -}}
+    {{- if $field.Parent.IsOmitEmpty}} {{else}} @required {{end -}}
+    {{if $field.Parent.IsList}}List<{{$field.DartType}}> {{else}}{{$field.DartType}} {{end -}}
+	{{$field.Name}},
+    
+    {{end}}
+  }) = _{{.Parent.Name}};
+
+  factory {{.Parent.Name}}.fromJson(Map<String, dynamic> json) => _${{.Parent.Name}}FromJson(json);
+}
+`))
+
+var dartEnumTemplate = template.Must(template.New("dartEnum").Parse(`import 'package:freezed_annotation/freezed_annotation.dart';
+
+enum {{.Name}} { {{range $value :=  .Values}}
   @JsonValue('{{$value}}')
   {{$value}},
   {{end}}
 }
 `))
 
-func generateDart(tdprotoInfo *codegen.TdInfo) {
+type DartClassField struct {
+	Name     string
+	DartType string
+	Parent   codegen.TdStructField
+}
+
+type DartClass struct {
+	Fields []DartClassField
+	Parent codegen.TdStruct
+}
+
+func generateDart(tdprotoInfo *codegen.TdPackage) error {
 	for _, tdEnum := range tdprotoInfo.GetEnums() {
-		dartEnumTemplate.Execute(os.Stdout, tdEnum)
+		err := dartEnumTemplate.Execute(os.Stdout, tdEnum)
+		if err != nil {
+			return err
+		}
 	}
+
+	dartClasses := generateDartClasses(tdprotoInfo)
+
+	for _, dartClass := range dartClasses {
+		err := dartClassTemplate.Execute(os.Stdout, dartClass)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func getDartTypeFromGoType(goType string) string {
+	primitiveType, ok := dartTypeMap[goType]
+	if !ok {
+		return goType
+	} else {
+		return primitiveType
+	}
+}
+
+func generateDartClasses(tdprotoInfo *codegen.TdPackage) (dartClasses []DartClass) {
+	for structName, structInfo := range tdprotoInfo.TdStructs {
+
+		newDartClass := DartClass{
+			Parent: structInfo,
+		}
+
+		for _, tdField := range structInfo.Fields {
+			newDartClass.Fields = append(newDartClass.Fields, DartClassField{
+				Name:     codegen.LowercaseFirstLetter(structName),
+				DartType: getDartTypeFromGoType(tdField.TypeStr),
+				Parent:   tdField,
+			})
+		}
+
+		dartClasses = append(dartClasses, newDartClass)
+	}
+
+	return
 }
 
 func main() {
@@ -37,5 +118,9 @@ func main() {
 		panic(err)
 	}
 
-	generateDart(tdprotoInfo.TdModels)
+	err = generateDart(tdprotoInfo.TdModels)
+	if err != nil {
+		panic(err)
+	}
+
 }
