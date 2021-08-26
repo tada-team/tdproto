02a2a450338a9c6c6f9c5ab68d6d0af8c0300d66
palage4a <vvnvvplg@gmail.com>
2021-08-27 01:52:03 +0300

    add subscription struct

---
+++ subscription.go
@@ -0,0 +1,17 @@
+package tdproto
+
+// Subscription - an entity that signifies the fact
+// of subscribing to the tariff of any team for a certain period
+// (not defined, in the case of the default tariff)
+type Subscription struct {
+	// Subscription id
+	Uid string `json:"uid"`
+	// Subscription activation time
+	Activated *time.Time `json:"activated,omitempty"`
+	// Subscription expiration time
+	Expires *time.Time `json:"expires,omitempty"`
+	// ID of the tariff for which the subscription is valid
+	TariffUid TariffPk `json:"tariff_uid,omitempty"`
+	// ID of the user who subscribed
+	UserUid *UserPk `json:"user_uid,omitempty"`
+}
