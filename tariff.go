654808bdc3d68854cf688038e25fb032091a2a1f
deniskelin <deniskelin@yandex.ru>
2021-10-25 16:37:37 +0300

    refactor: change tariff.go for new struct in billing-api

--- tariff.go
+++ tariff.go
@@ -1,5 +1,7 @@
 package tdproto
 
+import "time"
+
 // Tariff for teams
 type Tariff struct {
 	// Tariff id
@@ -22,4 +24,23 @@ type Tariff struct {
 
 	//maximum file size for uploading
 	MaxUploadFilesize int64 `json:"max_upload_filesize,omitempty"`
+}
+
+// TariffBilling struct of billing api
+type TariffBilling struct {
+	TariffId           int64      `json:"tariff_id"`
+	TariffName         string     `json:"tariff_name"`
+	FreeWorkplace      int64      `json:"free_workplace"`
+	DiskSpaceQuota     string     `json:"disk_space_quota"`
+	BillingFree        bool       `json:"billing_free"`
+	BillingFullTime    bool       `json:"billing_full_time"`
+	PeriodDays         int64      `json:"period_days"`
+	CostWorkplace      string     `json:"cost_workplace"`
+	Currency           string     `json:"currency"`
+	RecalcChangeTariff bool       `json:"recalc_change_tariff"`
+	MaxVoiceUser       int64      `json:"max_voice_user"`
+	MaxVideoUser       int64      `json:"max_video_user"`
+	DefaultTariff      bool       `json:"default_tariff"`
+	OpenDate           *time.Time `json:"open_date"`
+	CloseDate          *time.Time `json:"close_date"`
 }
