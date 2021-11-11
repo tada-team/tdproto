2302c095e3abf5f111c6fd1717494404bf2bdf7d
Vova Revenko <vova@revenko.org>
2021-11-11 14:33:22 +0300

    feat: added currency

---
+++ currency.go
@@ -0,0 +1,15 @@
+package tdproto
+
+// ISO 4217 currency code
+type Currency string
+
+const (
+	// Euro
+	EUR Currency = "EUR"
+
+	// Russian ruble
+	RUB Currency = "RUB"
+
+	// United States dollar
+	USD Currency = "USD"
+)
