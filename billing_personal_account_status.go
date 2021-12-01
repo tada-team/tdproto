cf123311f2da40c8c31d4d2f4d7809a72240b67b
deniskelin <deniskelin@yandex.ru>
2021-12-01 19:06:20 +0300

    fix(billing): change comments for documentation

--- billing_personal_account_status.go
+++ billing_personal_account_status.go
@@ -1,15 +1,15 @@
 package tdproto
 
-// Personal account status
+// PersonalAccountStatus account status
 type PersonalAccountStatus string
 
 const (
-	// Personal account is active
+	// ActiveAccount account is active
 	ActiveAccount PersonalAccountStatus = "PERSONAL_ACCOUNT_STATUS_ACTIVE"
 
-	// Personal account is under financial blocking
+	// SuspendedAccount account is under financial blocking
 	SuspendedAccount PersonalAccountStatus = "PERSONAL_ACCOUNT_STATUS_SUSPENDED"
 
-	// Personal account is under administrative blocking
+	// BlockedAccount account is under administrative blocking
 	BlockedAccount PersonalAccountStatus = "PERSONAL_ACCOUNT_STATUS_BLOCKED"
 )
