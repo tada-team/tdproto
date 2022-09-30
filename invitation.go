e292f3105a0bb7acacb0dac1639d5bd1d2e24186
fadinflame <berezin.albert@gmail.com>
2022-09-30 16:32:19 +0400

    feat(invitation): add invitation to a meeting

--- invitation.go
+++ invitation.go
@@ -2,8 +2,21 @@ package tdproto
 
 // Invitation to team
 type Invitation struct {
-	Uid     string            `json:"uid"`
-	Token   string            `json:"token"`
-	Created ISODateTimeString `json:"created"`
-	Qr      string            `json:"qr"`
+	Uid          string            `json:"uid"`
+	Token        string            `json:"token"`
+	Created      ISODateTimeString `json:"created"`
+	Qr           string            `json:"qr"`
+	InitialGroup string            `json:"initial_group,omitempty"`
+}
+
+// InvitationToMeeting Preview
+type InvitationToMeeting struct {
+	Description  string            `json:"description"`
+	DateStart    ISODateTimeString `json:"date_start"`
+	MeetingGroup string            `json:"meeting_group"`
+}
+
+type MeetingNewGuest struct {
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
 }
