4f8ae322f1dd5e0dffb047a8d47b09a6edc8541e
Victor Neznaykin <va.neznaykin@gmail.com>
2023-01-09 15:22:40 +0400

    try to update go.mod cache

--- server_chat_messages_history_cleared.go
+++ server_chat_messages_history_cleared.go
@@ -1,20 +1,23 @@
 package tdproto
 
+// NewServerChatHistoryCleared returns the new ServerChatHistoryCleared instance.
 func NewServerChatHistoryCleared(jid JID, lastCleared *string) (r ServerChatHistoryCleared) {
 	r.Name = r.GetName()
 	r.Params.JID = jid
 	if lastCleared != nil {
 		r.Params.LastCleared = lastCleared
 	}
+
 	return r
 }
 
-// ServerChatHistoryCleared
+// ServerChatHistoryCleared represents the event about clearing the chat messages history for user.
 type ServerChatHistoryCleared struct {
 	BaseEvent
 	Params serverChatHistoryClearedParams `json:"params"`
 }
 
+// GetName returns the name of ServerChatHistoryCleared instance.
 func (p ServerChatHistoryCleared) GetName() string { return "server.chat.history.cleared" }
 
 // Params of the server.chat.history.cleared event
