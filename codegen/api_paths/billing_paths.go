683ac6980cf687187ccfd33b33a4fe48b58856bc
fadinflame <berezin.albert@gmail.com>
2023-04-17 19:18:19 +0300

    feat(swagger codegen): add billing paths

--- codegen/api_paths/billing_paths.go
+++ codegen/api_paths/billing_paths.go
@@ -12,4 +12,29 @@ var BillingPaths = []PathSpec{
 			Description: "Get all tariffs.",
 		},
 	},
+	{
+		Path: "/api/v4/workplaces",
+		Get: &OperationSpec{
+			QueryStruct: tdproto.GetWorkplacesByPersonalAccountRequest{},
+			Response:    []tdproto.GetWorkplacesByPersonalAccountResponse{},
+			Description: "Get all workplaces.",
+		},
+		Put: &OperationSpec{
+			Request:     tdproto.AddWorkplacesOnPersonalAccountRequest{},
+			Response:    tdproto.ActivateWorkplacesOnPersonalAccountResponse{},
+			Description: "Add workplaces.",
+		},
+		Delete: &OperationSpec{
+			Request:     tdproto.DeleteUserFromWorkplaceRequest{},
+			Response:    tdproto.DeleteUserFromWorkplaceResponse{},
+			Description: "Delete workplaces.",
+		},
+	},
+	{
+		Path: "/api/v4/bill-teams",
+		Get: &OperationSpec{
+			Response:    tdproto.GetTeamOnPersonalAccountResponse{},
+			Description: "Get teams on personal account.",
+		},
+	},
 }
