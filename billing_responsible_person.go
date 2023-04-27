a09e4d8fcc3d9de4764976e0c362425892cccd80
Elena Sokolova <esokolova86@gmail.com>
2023-04-27 12:20:45 +0300

    update struct

--- billing_responsible_person.go
+++ billing_responsible_person.go
@@ -1,22 +1,24 @@
 package tdproto
 
 type ResponsiblePerson struct {
-	Id             string `json:"id"`
-	CounterpartyId string `json:"counterparty_id"`
-	FirstName      string `json:"first_name"`
-	LastName       string `json:"last_name"`
-	Patronymic     string `json:"patronymic,omitempty"`
-	Phone          string `json:"phone"`
-	Email          string `json:"email"`
+	Id          string  `json:"id"`
+	DisplayName string  `json:"display_name"`
+	FirstName   string  `json:"first_name"`
+	LastName    string  `json:"last_name"`
+	Patronymic  *string `json:"patronymic,omitempty"`
+	Phone       string  `json:"phone"`
+	Email       string  `json:"email"`
+	HeldPost    *string `json:"held_post,omitempty"`
 }
 
 type ResponsiblePersonCreateRequest struct {
-	CounterpartyId string `json:"counterparty_id"`
-	FirstName      string `json:"first_name"`
-	LastName       string `json:"last_name"`
-	Patronymic     string `json:"patronymic,omitempty"`
-	Phone          string `json:"phone"`
-	Email          string `json:"email"`
+	PersonalAccountId string  `json:"personal_account_id"`
+	FirstName         string  `json:"first_name"`
+	LastName          string  `json:"last_name"`
+	Patronymic        *string `json:"patronymic,omitempty"`
+	Phone             string  `json:"phone"`
+	Email             string  `json:"email"`
+	HeldPost          *string `json:"held_post,omitempty"`
 }
 
 type ResponsiblePersonCreateResponse struct {
@@ -24,13 +26,14 @@ type ResponsiblePersonCreateResponse struct {
 }
 
 type ResponsiblePersonUpdateRequest struct {
-	Id             string `json:"id"`
-	CounterpartyId string `json:"counterparty_id"`
-	FirstName      string `json:"first_name"`
-	LastName       string `json:"last_name"`
-	Patronymic     string `json:"patronymic"`
-	Phone          string `json:"phone"`
-	Email          string `json:"email"`
+	Id          string  `json:"id"`
+	DisplayName string  `json:"display_name"`
+	FirstName   string  `json:"first_name"`
+	LastName    string  `json:"last_name"`
+	Patronymic  *string `json:"patronymic,omitempty"`
+	Phone       string  `json:"phone"`
+	Email       string  `json:"email"`
+	HeldPost    *string `json:"held_post,omitempty"`
 }
 
 type ResponsiblePersonUpdateResponse struct {
@@ -42,9 +45,8 @@ type ResponsiblePersonGetRequest struct {
 }
 
 type ResponsiblePersonGetListRequest struct {
-	CounterpartyId string `json:"counterparty_id,omitempty"`
-	Limit          uint32 `json:"limit,omitempty"`
-	Offset         uint32 `json:"offset,omitempty"`
+	Limit  *uint32 `json:"limit,omitempty"`
+	Offset *uint64 `json:"offset,omitempty"`
 }
 
 type ResponsiblePersonGetResponse struct {
